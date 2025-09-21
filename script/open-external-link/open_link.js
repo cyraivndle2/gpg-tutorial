fe8c2d4534ffa6cad02084bcbc6a187828c4f2b2
cyraivndle2 <223052909+cyraivndle2@users.noreply.github.com>
2025-09-21 21:33:21 +0800

    外部链接 支持通过title手动标记外部链接

--- script/open-external-link/open_link.js
+++ script/open-external-link/open_link.js
@@ -3,11 +3,12 @@
  */
 document.addEventListener("DOMContentLoaded", function () {
   const origin = window.location.origin;
+  const externalMark = "点击前往外部站点";
 
   document.querySelectorAll("a[href]").forEach(function (el) {
     // 只处理非本站链接
     // @ts-ignore: el instanceof HTMLAnchorElement 
-    if (!el.href.startsWith(origin)) {
+    if (!el.href.startsWith(origin) || externalMark === el.title) {
       el.setAttribute("target", "_blank");
       el.setAttribute("rel", "noopener noreferrer");
     }
