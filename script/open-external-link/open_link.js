e3f0696308106a02ad2df50d0fb5e67c0b8ad2b2
cyraivndle2 <223052909+cyraivndle2@users.noreply.github.com>
2025-11-10 11:58:42 +0800

    [v2.1.1] 外部链接 避免覆盖已有提示

--- script/open-external-link/open_link.js
+++ script/open-external-link/open_link.js
@@ -8,10 +8,13 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll("a[href]").forEach(function (el) {
     // 只处理非本站或手动标记的链接
     // @ts-ignore: el instanceof HTMLAnchorElement 
-    if (!el.href.startsWith(origin) || externalLinkMark === el.title) {
+    const startsWithMark = el.title.indexOf(externalLinkMark) === 0;
+    if (el.href.indexOf(origin) !== 0 || startsWithMark) {
       el.setAttribute("target", "_blank");
       el.setAttribute("rel", "noopener noreferrer");
-      el.setAttribute("title", externalLinkMark);
+      if (!startsWithMark) {
+        el.setAttribute("title", externalLinkMark + (el.title ? ": " : "") + el.title);
+      }
     }
   });
 });
